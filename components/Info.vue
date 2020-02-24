8fbd861a002963a3cb0c84061a90334fcf304057
gomasiokomachi <yuno.thevamps@gmail.com>
2020-02-24 12:11:10 +0900

    infoComponent修正

--- components/Info.vue
+++ components/Info.vue
@@ -56,13 +56,13 @@ export default {
     flex-direction: column;
     flex-wrap: wrap;
     background: white;
-    width: 28%;
-    padding: 1% 1% 0;
+    width: 300px;
+    padding: 10px 10px 0;
     margin: auto;
     position: absolute;
     left: 0;
     right: 0;
-    bottom: calc(46%*1500/2000);
+    bottom: calc(45%*1500/2000);
     &::after{
         content: '';
         display: inline-block;
@@ -79,60 +79,102 @@ export default {
 .infoTitle{
     order: 2;
     text-align: center;
-    font-size: 20px;
+    font-size: 18px;
 }
 .infoDateTime{
     order: 1;
     display: flex;
     justify-content: flex-end;
     list-style: none;
-    font-size: 16px;
+    font-size: 14px;
 }
 .infoList{
     order: 3;
     list-style: none;
-    margin-top: 16px;
+    margin-top: 14px;
 }
 .infoListItem{
     display: flex;
-    padding: 16px;
+    padding: 14px;
     border-top: 1px #333 solid;
-    line-height: 46px;
-    font-size: 24px;
+    line-height: 40px;
+    font-size: 22px;
     &::before{
         content: '';
         display: block;
         background: pink;
-        width: 46px;
-        height: 46px;
+        width: 40px;
+        height: 40px;
         border-radius: 50%;
-        margin-right: 16px;
+        margin-right: 14px;
     }
 }
-    @media screen and (min-width:1375px){
-        .infoArea::after{
-            width: 120px;
-            height: 120px;
-            bottom: -66px;
-            right: -120px;
+    @media screen and (min-width:1200px){
+        .infoArea{
+            width: 320px;
+            &::after{
+                width: 120px;
+                height: 120px;
+                bottom: -66px;
+                right: -120px;
+            }
         }
         .infoTitle{
-            font-size: 24px;
-        }
-        .infoDateTime{
             font-size: 20px;
         }
+        .infoDateTime{
+            font-size: 16px;
+        }
         .infoList{
-            margin-top: 20px;
+            margin-top: 16px;
         }
         .infoListItem{
-            padding: 20px;
-            line-height: 50px;
-            font-size: 28px;
+            padding: 16px;
+            line-height: 42px;
+            font-size: 24px;
             &::before{
-                width: 50px;
-                height: 50px;
-                margin-right: 20px;
+                width: 42px;
+                height: 42px;
+                margin-right: 16px;
+            }
+        }
+    }
+    @media screen and (max-width:1000px){
+        .infoArea{
+            width: 260px;
+            &::after{
+                width: 60px;
+                height: 60px;
+                bottom: -33px;
+                right: -60px;
+            }
+        }
+        .infoTitle{
+            font-size: 14px;
+        }
+        .infoDateTime{
+            font-size: 10px;
+        }
+        .infoList{
+            margin-top: 10px;
+        }
+        .infoListItem{
+            padding: 10px;
+            line-height: 36px;
+            font-size: 18px;
+            &::before{
+                width: 36px;
+                height: 36px;
+                margin-right: 10px;
+            }
+        }
+    }
+    @media screen and (max-width:768px){
+        .infoArea{
+            left: 50px;
+            right: auto;
+            &::after{
+                display: none;
             }
         }
     }
