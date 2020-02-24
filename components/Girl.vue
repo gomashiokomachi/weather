be8b77feff8c49ad64089a492cff88ad1780d6f0
gomasiokomachi <yuno.thevamps@gmail.com>
2020-02-24 14:50:27 +0900

    girlComponent追加

---
+++ components/Girl.vue
@@ -0,0 +1,129 @@
+<template>
+
+<svg class='girl' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" enable-background="new 0 0 300 300" xml:space="preserve">
+<g id="pillow_1_">
+	<path id="pillow" fill="#FFFCED" stroke="#FFFCED" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M180.295,261.729c0,0,6.316,6.765,21.243,6.262s31.194-17.219,32.872-31.754c0,0,11.628-7.155,10.957-38.461
+		c0,0-9.168-20.127-58.364-21.245c0,0-16.547-7.826-34.884-5.143L180.295,261.729z"/>
+</g>
+<g id="lower">
+	<path id="feet" fill="#FFF4F0" d="M174.928,249.543c0,0,13.703,8.328,11.883,18.588c0,0-1.599,4.109-10.989,0.756
+		C166.431,265.532,171.015,249.208,174.928,249.543z"/>
+	<path id="pants" fill="#706861" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M79.22,246.188c0,0-5.697,10.366,1.254,18.561c0,0,4.783,5.903,21.107,2.616c0,0-1.006,19.638,34.213,15.247
+		c35.22-4.391,40.364-11.324,40.364-11.324s5.254-3.632-1.79-18.559L79.22,246.188z"/>
+</g>
+<g id="cushion">
+	<path id="cushion_1_" fill="#FAC0C9" stroke="#FAC0C9" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M135.124,191.067c0,0-10.421-7.864-23.267-3.429c0,0-8.375,5.385-11.953,14.777c0,0-17.591,13.062-25.12,40.382
+		c0,0-0.932,19.042,41.556,32.349c0,0,13.975,13.308,36.338,16.438c0,0,13.53-11.406,20.462-31.643c0,0,4.135-9.616,8.049-14.87
+		s10.035-18.086-4.472-29.154C176.717,215.917,160.28,202.025,135.124,191.067z"/>
+</g>
+<g id="leftHand">
+	
+		<path id="leftHand_1_" fill="#FFF4F0" stroke="#FFF4F0" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M149.323,244.919c0,0-3.13,6.862,0.559,13.682c0,0,2.148,4.484,6.29,0c0,0,3.103,1.993,4.779-1.268c0,0,3.495,1.517,4.957-1.49
+		S153.711,238.87,149.323,244.919z"/>
+</g>
+<g id="upper">
+	<path id="neck" fill="#FFECE6" stroke="#FFECE6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M120.589,140.5c0,0,1.342,4.994,1.566,8.124c0,0-4.025,2.078-1.566,7.206c2.459,5.129,16.258,5.186,20.428-1.122
+		c0,0,2.438-3.409-1.42-5.966c0,0-0.448-3.242,0-8.242H120.589z"/>
+	<path id="sweats" fill="#F2E5AD" stroke="#F2E5AD" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M120.589,148.132c0,0-1.487,5.375,2.74,8.219c4.226,2.845,11.179,1.213,14.03-0.792c2.853-2.002,3.681-4.184,3.658-7.427
+		c0,0,3.26-2.372,9.396,2.665c6.136,5.036,23.371,20.369,28.023,51.004c4.653,30.636-11.767,55.457-11.767,55.457
+		s-16.946-3.623-19.177-11.065s-13.375-36.341-13.375-36.341s-2.013-8.833,1.006-18.784c0,0-10.421-7.864-23.267-3.429
+		c0,0-8.375,5.385-11.953,14.777c0,0-1.179-9.148-11.144-9.56C88.76,192.856,102.365,146.119,120.589,148.132z"/>
+</g>
+<g id="phone">
+	<path id="phone_1_" fill="#F0666F" stroke="#F0666F" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M72.512,198.671c0,0,2.124,17.218,5.367,35.331c0,0,0.807,5.815,6.888,7.939c6.081,2.123,18.043,5.479,21.398,0
+		c3.354-5.479-3.466-41.706-3.466-41.706s-1.603-6.822-8.348-8.498s-19.939-4.509-21.839,0.373
+		C72.512,192.11,71.867,193.74,72.512,198.671z"/>
+</g>
+<g id="rightHand">
+	
+		<path id="rightHand_1_" fill="#FFF4F0" stroke="#FFF4F0" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M76.537,208.9c0,0,10.843,1.901,10.621,6.933c0,0,0.168,2.347-4.809,3.801c0,0,1.459,3.07,0.355,5.088c0,0,0.578,2.572-1.398,4.025
+		c0,0-0.131,2.999-3.653,2.869s-9.782-3.428-7.435-15.895C70.219,215.722,72.288,208.452,76.537,208.9z"/>
+</g>
+<g id="head">
+	<path id="hair" fill="#C7B299" stroke="#C7B299" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M177.611,34.614c0,0,0.392-13.16-22.138-12.322c-22.53,0.838-23.033-9.895-19.679-13.417c0,0-6.149,4.36-4.248,10.566
+		c0,0-33.601-2.013-57.024,27.337c0,0-9.839,11.851-18.224,3.409c0,0-0.093,10.511,10.099,13.865c0,0-8.422,9.441-6.913,35.412
+		c0,0,2.348,11.473-4.193,15.871c0,0,1.845,2.537,5.702,1.79c3.857-0.747,1.973,25.314,20.666,28.584c0,0-1.689-2.606-1.098-4.733
+		c4.651-0.897,8.198,4.94,8.198,4.94s8.2-0.468,10.25-4.94c6.819,5.367,10.381,3.025,14.199-1.63l32.2-3.222
+		c0,0-2.906,14.518,9.169,5.353c0,0,5.76-5.869,5.145,1.287c0,0,9.057-1.322,15.205-11.591c0,0,1.534,7.604-3.537,10.996
+		c-5.072,3.391,10.124-1.527,19.191-10.996c0,0,0.967,6.308,4.921,6.182c0,0,7.619-6.293,13.079-19.04c0,0,3.273,0.324,5.702-2.273
+		c2.43-2.597-10.255-5.406-3.689-9.056c5.199-2.891,5.199-5.367,5.199-5.367s-13.677-5.236-12.396-17.442
+		c0,0,8.426-0.671,9.992-6.708c0,0-13.175-3.522-13.938-16.771C199.451,60.698,196.116,32.848,177.611,34.614z"/>
+	<path id="face" fill="#FFF4F0" stroke="#FFF4F0" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M173.586,39.454c0,0-23.704-19.842-71.557-3.1c-47.856,16.743-29.098,70.221-25.047,71.991c0,0-6.038,32.185,37.568,33.079
+		c43.607,0.896,57.471-9.615,60.377-14.534c0,0,9.259,1.006,10.946-12.075c0,0,0.308-3.467-2.077-5.144
+		C183.797,109.67,209.03,72.326,173.586,39.454z"/>
+	<path id="bangs" fill="#C7B299" stroke="#C7B299" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M70.274,65.997c0,0-7.884,23.017,9.363,24.96c0,0-9.194-9.184-3.1-17.961c0,0-2.685,12.412,8.496,14.313
+		c0,0-4.319-6.143,2.61-15.207c0,0,7.733-10.398,13.938-4.025c0,0,13.417-10.845,20.684,18.561c0,0,4.138-5.692,3.58-13.244
+		c0,0,10.872,2.027,3.814,14.287c0,0,12.36-1.927,10.533-14.287c0,0,7.79,23.179,27.636,16.002c0,0,7.155,22.51-4.25,17.367
+		c0,0,0.95,4.952,8.386,1.581c0,0,8.554,7.513,14.145,6.991c0,0,28.025-41.928-7.343-76.404c0,0-17.592-19.354-64.774-8.969
+		C113.992,29.963,79.637,35.96,70.274,65.997z"/>
+	<path id="rightEye" fill="#998675" stroke="#998675" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M103.22,102.405c-2.807-1.87-8.154-4.311-14.684-1.823c0,0-2.379,4.521-0.215,8.286c2.162,3.766,8.51,3.114,10.296,2.366
+		C103.426,109.222,103.22,102.405,103.22,102.405z"/>
+	<path id="leftEye" fill="#998675" stroke="#998675" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M156.751,109.437c-7.347-6.68-13.608-5.9-17.306-4.343c0,0-4.015,6.959,2.524,10.676c5.505,3.13,10.761,1.6,12.86-0.407
+		C157.052,113.234,156.751,109.437,156.751,109.437z"/>
+	<g id="lights">
+		<circle id="rightLight" fill="#FFFFFF" cx="99.12" cy="107.658" r="2.012"/>
+		<circle id="leftLight" fill="#FFFFFF" cx="151.447" cy="114.366" r="2.013"/>
+	</g>
+	<g id="eyelids">
+		<path id="rightEyelid" fill="none" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+			M105.354,104.097c0,0-9.196-9.353-20.993-1.302"/>
+		
+			<path id="rightDoubleEyelid" fill="none" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+			M104.414,101.885c0,0-6.537-8.019-20.052-2.359"/>
+		<path id="leftEyelid" fill="none" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+			M135.878,107.293c0,0,10.291-9.838,23.322,4.612"/>
+		
+			<path id="leftDoubleEyelid" fill="none" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+			M137.359,103.8c0,0,11.266-6.214,21.287,5.55"/>
+	</g>
+	<g id="cheek">
+		
+			<line id="rrCheek" fill="none" stroke="#FAB6B7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="80.562" y1="109.222" x2="81.66" y2="111.012"/>
+		
+			<line id="rlCheek" fill="none" stroke="#FAB6B7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="83.246" y1="109.222" x2="84.343" y2="111.012"/>
+		
+			<line id="lrCheek" fill="none" stroke="#FAB6B7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="157.039" y1="117.272" x2="158.137" y2="119.062"/>
+		
+			<line id="llCheek" fill="none" stroke="#FAB6B7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="159.723" y1="117.272" x2="160.82" y2="119.062"/>
+	</g>
+	<path id="mouth" fill="none" stroke="#706861" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M134.733,132.05c0,0-6.654-0.465-10.176,4.007"/>
+</g>
+</svg>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+    .girl{
+        width: 24%;
+        position: absolute;
+        right: 4%;
+        bottom: calc(30%*1500/2000);
+    }
+
+    @media screen and (max-width:768px){
+        .girl{
+            width: 34%;
+            right: 6%;
+        }
+    }
+
+</style>
