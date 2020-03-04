ac4bf0e5f2a7aa8fc7ef577486b5a60decdda740
gomasiokomachi <yuno.thevamps@gmail.com>
2020-03-04 18:02:17 +0900

    windowComponent修正

--- components/Window.vue
+++ components/Window.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div class="windowArea"></div>
+<div class="windowArea" v-bind:class="[TIME, MAIN_VIEW]"></div>
 
 </template>
 
@@ -9,31 +9,40 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
-    computed: { //weatherItemが変わったら実行される
+    computed: {
+        TIME: () => {
+            let time = new Date()
+            let hour = time.getHours()
+            if(hour > 17 || hour < 6) {
+                return 'night'
+            } else if(hour > 15) {
+                return 'evening'
+            } else {
+                return 'daytime'
+            }
+        },
         MAIN_VIEW() {
             let main = this.weatherItem && this.weatherItem.weather[0].main
-            if(main === 'Clear') {
-                return 'はれ'
-            } else if(main === 'Clouds') {
-                return 'くもり'
+            if(main === 'Clouds') {
+                return 'clouds'
             } else if(main === 'Rain') {
-                return 'あめ'
+                return 'rain'
             } else if(main === 'Snow') {
-                return 'ゆき'
+                return 'snow'
             } else {
-                return main
+                return
             }
         },
-        WIND_VIEW() {
-            const speed = this.weatherItem && this.weatherItem.wind.speed
-            if(speed > 10) {
-                return '風強い！'
-            } else if(speed > 3) {
-                return '風普通'
-            } else {
-                return '風弱い'
-            }
-        },
+//        WIND_VIEW() {
+//            const speed = this.weatherItem && this.weatherItem.wind.speed
+//            if(speed > 10) {
+//                return ''
+//            } else if(speed > 3) {
+//                return ''
+//            } else {
+//                return ''
+//            }
+//        },
         ...mapState({
               weatherItem: state => state.api.weatherItem
           })
@@ -52,6 +61,36 @@ export default {
 
 <style scoped>
 
-
+    .windowArea{
+        width: 20%;
+        height: 40%;
+        position: absolute;
+        left: 0;
+        bottom: calc(60%*1500/2000);
+        z-index: -1;
+        &:after{
+            content: '';
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .daytime{
+        background: #DAF1FF;
+    }
+    .evening{
+        background: #f4a460;
+    }
+    .night{
+        background: #003366;
+    }
+/*
+    .clouds:after, .rain:after{
+        background: #778899;
+    }
+    .snow:after{
+        background: white;
+    }
+*/
 
 </style>
