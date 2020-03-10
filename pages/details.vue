619f3c9f244c51b0f463857b2baf17f5b0091b0f
gomasiokomachi <yuno.thevamps@gmail.com>
2020-03-10 12:45:33 +0900

    bgをbeforeに変更

--- pages/details.vue
+++ pages/details.vue
@@ -28,7 +28,24 @@ export default {
 
     .container{
         display: flex;
-        background: url("/bbg.jpg");
+        position: relative;
+        &::before{
+            content: '';
+            background: url("/bbg.jpg");
+            width: 100vw;
+            height:100vh;
+            margin-left: calc( ( ( 100% - 100vw ) / 2 ) );
+            margin-right: calc( ( ( 100% - 100vw ) / 2 ) );
+            position: absolute;
+            z-index: -10;
+        }
+    }
+
+    @media screen and (min-width:1200px){
+        .container{
+            width: 1200px;
+            margin: 0 auto;
+        }
     }
 
 </style>
